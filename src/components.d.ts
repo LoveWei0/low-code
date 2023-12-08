a0de0b2fb6c343fe659282549d22f877b7350079
lovewei0 <xdw666888@qq.com>
2023-12-08 14:45:29 +0800

    fix(.gitignore): update

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,10 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    Toolbar: typeof import('./components/Toolbar.vue')['default']
+    ElButton: (typeof import('element-plus/es'))['ElButton']
+    ElSwitch: (typeof import('element-plus/es'))['ElSwitch']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
+    Toolbar: (typeof import('./components/Toolbar.vue'))['default']
   }
 }
